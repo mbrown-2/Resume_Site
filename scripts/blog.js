58fc1f506ca9774cd1ffdf51aa6c9d64671278ca
mbrown-2 <michaelbrown.5619@gmail.com>
2022-07-26 01:40:25 -0700

    time to finish this

---
+++ scripts/blog.js
@@ -0,0 +1,8 @@
+function main() {
+
+}
+
+window.addEventListener('DOMContentLoaded', main);
+
+// tip: add localStorage at the end
+// helpful resource: https://www.javaguides.net/2020/11/javascript-crud-example-tutorial.html
