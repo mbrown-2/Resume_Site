a645bc869b707b97221099d833b72f4253cba68c
mbrown-2 <michaelbrown.5619@gmail.com>
2022-07-26 17:44:42 -0700

    all that's left: input localStorage

--- scripts/blog.js
+++ scripts/blog.js
@@ -2,10 +2,11 @@
 let dialogBox = document.getElementById('post_dialog');
 let addPost = document.getElementById('add_post');
 let cancelPost = document.getElementById('cancel_post');
+let editPost = document.getElementById('edit_post');
+// editPost.style.display = 'none';
 let savePost = document.getElementById('save_post');
 let messageNotif = document.getElementById('message_notif');
 let postList = document.getElementById('post_list');
-
 let subtree_p3 = document.getElementById('post_dialog');
 let userInputs = subtree_p3.getElementsByTagName('input');
 let title = userInputs[0];
@@ -15,11 +16,18 @@ let summary = userInputs[2];
 
 // Helpers
 function openBox() {
+    savePost.style.dislay = 'inline';
+    editPost.style.display = 'none';
     dialogBox.show();
 }
 
 function closeBox() {
+    savePost.style.display = 'inline';
+    editPost.style.display = 'none';
     dialogBox.close();
+    title.value = '';
+    date.value = '';
+    summary.value = '';
 }
 
 // Read data
@@ -50,11 +58,11 @@ let createPost = () => {
     let postData = readPostData();
     postList.innerHTML += `
     <div class="entry">
-        <h2>${postData['title']}</h2>
-        <h3>${postData['date']}</h3>
-        <p>${postData['summary']}</p>
-        <button id="edit">Edit</button>
-        <button id="delete">Delete</button>
+        <h2 class="title">${postData['title']}</h2>
+        <h3 class="date">${postData['date']}</h3>
+        <p class="summary">${postData['summary']}</p>
+        <button class="edit" onclick="changePost(this)">Edit</button>
+        <button class="delete" onclick="removePost(this)">Delete</button>
         <br /><br />
     </div>
     `;
@@ -63,8 +71,38 @@ let createPost = () => {
     summary.value = '';
 }
 
-// Post deletion
-let deletePost = (entry) => {};
+// Delete a post
+let removePost = (element) => {
+    element.parentElement.remove();
+};
+
+// Edit a post
+let changePost = (element) => {
+    // Modify visible buttons
+    savePost.style.display = 'none';
+    editPost.style.display = 'inline';
+
+    // Obtain parent and send contents to dialog inputs
+    let parent = element.parentElement;
+    title.value = parent.querySelectorAll('.title')[0].innerHTML;
+    date.value = parent.querySelectorAll('.date')[0].innerHTML;
+    summary.value = parent.querySelectorAll('.summary')[0].innerHTML;
+    dialogBox.show();
+
+    // transfer data back into the parent, reset the button appearances, and empty dialog
+    editPost.addEventListener('click', function() {
+        let postData = readPostData();
+        parent.querySelectorAll('.title')[0].innerHTML = postData['title'];
+        parent.querySelectorAll('.date')[0].innerHTML = postData['date'];
+        parent.querySelectorAll('.summary')[0].innerHTML = postData['summary'];
+        savePost.style.display = 'inline';
+        editPost.style.display = 'none';
+        title.value = '';
+        date.value = '';
+        summary.value = '';
+        dialogBox.close();
+    });
+};
 
 // Main function for window object to call
 // Again, breaking everything into separate functions for a granular approach
@@ -79,3 +117,4 @@ window.addEventListener('DOMContentLoaded', main);
 // tip: add localStorage at the end
 
 // delete and edit buttons
+
